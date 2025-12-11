f453065fa5446c5e58cef3b7f0cdbfd80c877925
Raavee Singh <gdg1465@gmail.com>
2025-12-11 16:07:44 +0530

    lessgo

---
+++ Day 1/6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    while(num<5){
+        printf("I am smart. You are dumb.\n");
+        num=num+1;
+    }
+    do{
+        printf("I am smart. You are dumb.\n");
+        num=num+1;
+    }while(num<5);
+    return 0;
+}
